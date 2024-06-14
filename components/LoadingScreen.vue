b8f9b6a39b70684dc1aafbc2aa439f85c9ce1a02
egrazhs <egrazhs@gmail.com>
2024-06-14 02:13:14 -0600

    estilos y arreglando toques finales a Read usuarios

---
+++ components/LoadingScreen.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <!-- Resto del contenido -->
+
+    <LoadingSpinner v-if="loading" mensaje="Cargando detalles del usuario..."></LoadingSpinner>
+  </div>
+</template>
+
+<script setup>
+  import LoadingSpinner from '~/components/LoadingSpinner.vue';
+  import { ref, onMounted } from 'vue';
+
+  const loading = ref(true);
+
+  onMounted(() => {
+    // Lógica para cargar datos
+    loading.value = false;
+  });
+</script>
---
+++ components/LoadingSpinner.vue
@@ -0,0 +1,23 @@
+<template>
+	<v-container class="d-flex justify-center align-center" style="height: 100vh;">
+		<div class="text-center">
+			<v-progress-circular indeterminate size="64"></v-progress-circular>
+			<p>{{ mensaje }}</p>
+		</div>
+	</v-container>
+</template>
+
+<script setup>
+	import { defineProps } from 'vue';
+
+	const props = defineProps({
+		mensaje: {
+			type: String,
+		default: 'Cargando...'
+		}
+	});
+</script>
+
+<style scoped>
+/* Estilos específicos según tus preferencias */
+</style>
