967d2ebfac3ee4e501bb463f67a776273cde423e
egrazhs <egrazhs@gmail.com>
2024-08-30 17:25:00 -0600

    Deploy a hosting; Cambio de rutas de las imagenes en mainNav

--- components/mainNav.vue
+++ components/mainNav.vue
@@ -1,22 +1,22 @@
 <template>
 	<article class="bg-blue-800 text-white flex flex-col justify-between">
 		<div>
-            <NavItem to="/" imgSource="clipboard.svg" text="Inicio" />
-            <NavItem to="/usuarios" imgSource="user.svg" text="Usuarios" />
-            <NavItem to="/clientes" imgSource="user-group.svg" text="Clientes" />
-            <NavItem to="/proveedores" imgSource="briefcase.svg" text="Proveedores" />
-            <NavItem to="/vehiculos" imgSource="truck.svg" text="Vehiculos" />
-            <NavItem to="/productos" imgSource="cog.svg" text="Productos" />
-            <NavItem to="/sucursales" imgSource="building-storefront.svg" text="Sucursales" />
-            <NavItem to="/arqueos" imgSource="presentation-chart.svg" text="Arqueos" />
-            <NavItem to="/remisiones" imgSource="banknote.svg" text="Remisiones" />
-            <NavItem to="/abonos" imgSource="banknote.svg" text="Abonos" />
-            <NavItem to="/ejemplo" imgSource="window.svg" text="Ejemplo de E.C." />		
+            <NavItem to="/" imgSource="/images/heroicons/clipboard.svg" text="Inicio" />
+            <NavItem to="/usuarios" imgSource="/images/heroicons/user.svg" text="Usuarios" />
+            <NavItem to="/clientes" imgSource="/images/heroicons/user-group.svg" text="Clientes" />
+            <NavItem to="/proveedores" imgSource="/images/heroicons/briefcase.svg" text="Proveedores" />
+            <NavItem to="/vehiculos" imgSource="/images/heroicons/truck.svg" text="Vehiculos" />
+            <NavItem to="/productos" imgSource="/images/heroicons/cog.svg" text="Productos" />
+            <NavItem to="/sucursales" imgSource="/images/heroicons/building-storefront.svg" text="Sucursales" />
+            <NavItem to="/arqueos" imgSource="/images/heroicons/presentation-chart.svg" text="Arqueos" />
+            <NavItem to="/remisiones" imgSource="/images/heroicons/banknote.svg" text="Remisiones" />
+            <NavItem to="/abonos" imgSource="/images/heroicons/banknote.svg" text="Abonos" />
+            <NavItem to="/ejemplo" imgSource="/images/heroicons/window.svg" text="Ejemplo de E.C." />		
         </div>
 
 		<div>
 			<div id="cerrar_sesion" class="py-4 px-2 md:px-8 hover:bg-blue-600 cursor-pointer flex items-center">
-                <img src="~/assets/images/heroicons/arrow-right-on-rectangle.svg" class="w-6 h-6" draggable="false">
+                <img src="/images/heroicons/arrow-right-on-rectangle.svg" class="w-6 h-6" draggable="false">
                 <span class="text-xs ml-1"> Cerrar Sesión</span>
             </div>
 		</div>
